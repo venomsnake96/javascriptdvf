9e3b6cfe74ff7a5c6d8e5beb1fb2bf4577ab6b11
VenomSnake <claudiojaimez.sec3@gmail.com>
2024-07-08 21:35:33 -0600

    add 3 exercice of while

---
+++ n3/script.js
@@ -0,0 +1,35 @@
+// N.1
+
+// let number = parseInt(prompt("Por favor, ingresa un numero:"));
+
+// var contador = 1;
+
+// while(contador <= number){
+//     if(contador % 5 === 0){
+//         console.log(contador)
+//     }
+//     contador++
+// }
+
+
+// N.2
+
+// let number1 = parseInt(prompt("Ingresa un numero entre 1 y 50"))
+// let number2 = parseInt(prompt("ingresa un segundo numero entre 1 y 50"))
+
+// if ((number1 < 1 || number1 > 50) || (number2 < 1  || number2 > 50)){
+//     alert("Ambos numeros deben estar entre 1 y 50, recarga la pagina para ingresar los datos nuevamente")
+// }else {
+//     let counter = 1 
+
+//     while (counter <= 50){
+//         if(counter === number1 || counter === number2){
+//             console.log(`${counter} Loteria!`);
+//         }else{
+//             console.log(counter)
+//         }
+//         counter++
+//     }
+// }
+
+
